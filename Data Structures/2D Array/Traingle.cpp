97194e8c0df5fa961f54f4a8d9d5875b7098c711
amishajha <jhaamisha096@gmail.com>
2022-06-17 13:04:18 +0530

    added BT qs

---
+++ Data Structures/2D Array/Traingle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+// Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+
+
+
+ int minimumTotal(vector<vector<int>>& triangle) {
+
+    int num = 
+     for(int i = 0;i<triangle.size();i++){
+        for(int j = 0;j<triangle.size();j++){
+
+        }
+     }   
+        
+        
+}
+
+
+
+int main(){
+
+}
